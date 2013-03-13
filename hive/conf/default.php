fa7bb43f323d7ef6d3fd0cd5c46891a3af9d96f8
Arnaud Lemaire <dev@norad.fr>
2013-03-13 18:44:21 +0100

    add hive info

---
+++ hive/conf/default.php
@@ -0,0 +1,6 @@
+<?php
+require dirname(__FILE__)."/../agtpl/conf/default.php";
+$conf['agtpl_logo']	= DOKU_TPL."/images/hive.png";
+$conf['main_top'] = dirname(__FILE__)."/../include/top.php";
+$conf['use_left']		= true;
+$conf['agtpl_body_type'] = "ct lt";
---
+++ hive/main.php
@@ -0,0 +1,2 @@
+<?php 
+include 'agtpl/agtpl.php';
